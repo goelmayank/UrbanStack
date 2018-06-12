b7ab33e91f8372bf7ce88dbe6bfedc5ee738a055
thenaughtychid <f2014768@pilani.bits-pilani.ac.in>
2018-06-12 13:28:32 +0530

    input

--- urbanstack-network/lib/logic.js
+++ urbanstack-network/lib/logic.js
@@ -1,103 +1,61 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 'use strict';
 
-/* global getAssetRegistry getqrpassRegistry getFactory */
-
 /**
- *
- * @param {org.urbanstack.StartTrip} tt - model instance
- * @transaction
- */
-
-async function StartTrip(tt) {
-    console.log('StartTrip');
-
-    //update the carrier of the Trip to Transit Provider
-    if (!tt.qrpass.carrier) {
-        tt.qrpass.carrier = tt.mobilityAsset.owner;
-    } else {
-        throw new Error('Trip is already being carried by a Transit Provider');
-    }
-
-    //save the Trip
-    const qr = await getAssetRegistry('org.urbanstack.Trip');
-    await qr.update(tt.qrpass);
-
-    //add the Trip to Trasit Provider QRPasses[]
-    if (tt.mobilityAsset.owner.qrpasses) {
-        tt.mobilityAsset.owner.qrpasses.push(tt.qrpass);
-    } else {
-        tt.mobilityAsset.owner.qrpasses = [tt.qrpass];
-    }
-
-    //save Trasit Provider
-    const tp = await getParticipantRegistry('org.urbanstack.TransitProvider');
-    await tp.update(tt.mobilityAsset.owner);
-}
-
-/**
- *
- * @param {org.urbanstack.EndTrip} tt - model instance
- * @transaction
- */
-async function EndTrip(tt) {
-    console.log('EndTrip');
-
-    //update the carrier of the Trip to null
-    if (tt.qrpass.carrier) {
-        tt.qrpass.carrier = null;
-    } else {
-        throw new Error('Trip is not being carried by any Transit Provider');
-    }
-
-    //save the Trip
-    const qr = await getAssetRegistry('org.urbanstack.Trip');
-    await qr.update(tt.qrpass);
-
-    //remove the Trip from Transit Provider QRPasses[]
-    tt.mobilityAsset.owner.qrpasses = tt.mobilityAsset.owner.qrpasses
-        .filter(function(qrpass) {
-            return qrpass.qrpassId !== tt.qrpass.qrpassId;
-        });
-
-    //save Transit Provider
-    const user = await getParticipantRegistry('org.urbanstack.TransitProvider');
-    await user.update(tt.mobilityAsset.owner);
-
-    // Emit the event TripCreated
-    var event = factory.newEvent(NS, 'TripCreated');
-    event.tripId = tripId;
-    emit(event);
-}
-
-/**
- * Book Trip Transaction
+ * Create vPassenger Transaction
  * @param {org.urbanstack.CreateTrip} tripData
  * @transaction
- *
- * Check for the validity of the start_time - throw error
- * Create the Trip asset
- *    Set the tripId, tripNumber
- *    Create an instance of the 'route' Concept
- *    Set the data on 'route' Concept
- *    Set the trip asset route = 'route' concept
- * Emit TripCreated Event
- * Add the trip asset to the registry
+ * Input:
+ * TripLeg[] tentativeTripLegs
+ * String passengerKey
  */
 function createTrip(tripData) {
 
+    // Get the Asset Registry
+
+    return getAssetRegistry('org.urbanstack.vPassenger')
+
+    .then(function(vPassengerRegistry) {
+        // Now add the vPassenger - global function getFactory() called
+        var factory = getFactory();
+
+        var NS = 'org.urbanstack';
+
+        var vPassengerId = parseInt(Math.random() * 100000000);
+        var vPassenger = factory.newResource(NS, 'vPassenger', vPassengerId);
+
+        tripData.tentativeTripLegs.forEach(tripLeg => {
+            vPassenger.tentativeTripLegs.push(tripLeg);
+            return getParticaipantRegistry('org.urbanstack.TransitProvider')
+                .then(function(transitProviderRegistry) {
+                    return transitProviderRegistry.get(tripLeg.transitProviderKey);
+                }).then(function(transitProvider) {
+                    if (!transitProvider) throw new Error("Transit Provider with: " + tripLeg.transitProviderKey, " Not Found!!!");
+                    transitProvider.tentativeTripLegs.push(tripLegId);
+                });
+        });
+
+        vPassenger.passengerKey = tripData.passengerKey;
+
+        // Emit the event TripCreated
+        var event = factory.newEvent(NS, 'TripCreated');
+        event.vPassengerId = vPassengerId;
+        emit(event);
+
+        // Add to registry
+        return vPassengerRegistry.add(vPassenger);
+    });
+}
+
+/**
+ * Book vPassenger Transaction
+ * @param {org.urbanstack.ConfirmTripLeg} tripData
+ * @transaction
+ * Input:
+ * DateTime start_time
+ * TripLeg[] tentativeTripLegs
+ * String passengerKey
+ */
+function ConfirmTripLeg(tripData) {
     /**
      * 1. Validate the start_time data
      * If the date is a past date then throw an error
@@ -107,56 +65,35 @@ function createTrip(tripData) {
     if (schedTime < timeNow) {
         throw new Error("start_timed time cannot be in the past!!!");
     }
-
-    // Get the Asset Registry
-
-    return getAssetRegistry('org.urbanstack.Trip')
-
-    .then(function(tripRegistry) {
-        // Now add the Trip - global function getFactory() called
-        var factory = getFactory();
-
-        var NS = 'org.urbanstack';
-
-        // generate the trip ID
-        // Set the tripNumber, tripId ...
-        var tripId = parseInt(Math.random() * 100000000);
-        var trip = factory.newResource(NS, 'Trip', tripId);
-
-        tripData.tentativeTripLegs.forEach(tripleg => {
-            trip.tentativeTripLegs.push(tripleg);
-        });
-
-        trip.passengerKey = tripData.passengerKey;
-
-        // Emit the event TripCreated
-        var event = factory.newEvent(NS, 'TripCreated');
-        event.tripId = tripId;
-        emit(event);
-
-        // Add to registry
-        return tripRegistry.add(trip);
-    });
 }
 
 /**
- * Create Trip Transaction
+ * Create vPassenger Transaction
  * @param {org.urbanstack.BusScan} tripData
  * @transaction
  * 
+ * Input
+ * String vPassengerId
+ * String transitProviderKey
+ * String MiD
  * **/
 function BusScan(tripData) {
     var transitProviderRegistry = {}
+    var passengerKey;
+    var tripLegId;
+    var fare;
+    var passengerRegistry = {}
+
+    var vPassengerRegistry = {}
+
     return getParticipantRegistry('org.urbanstack.cto.TransitProvider').then(function(registry) {
         transitProviderRegistry = registry
         return transitProviderRegistry.get(tripData.transitProviderKey);
     }).then(function(transitProvider) {
         if (!transitProvider) throw new Error("Transit Provider with: " + tripData.transitProviderKey, " Not Found!!!");
-        var passengerKey;
-        var tripLegId;
-        var fare;
+
         transitProvider.tentativeTripLegs.forEach(keyValue => {
-            if (keyValue.tripId == tripData.tripId) {
+            if (keyValue.vPassengerId == tripData.vPassengerId) {
                 keyValue.tripLeg.start_time = tripData.timestamp;
                 keyValue.tripLeg.MiD = tripData.MiD;
                 keyValue.tripLeg.TripLegStatus = "STARTED";
@@ -166,55 +103,88 @@ function BusScan(tripData) {
                 fare = keyValue.tripLeg.fare;
             }
         });
+        transitProvider.balance += fare;
 
-        var passengerRegistry = {}
-        return getParticipantRegistry('org.urbanstack.cto.Passenger').then(function(registry) {
-            passengerRegistry = registry
-            return passengerRegistry.get(passengerKey);
-        }).then(function(passenger) {
-            if (!passenger) throw new Error("Passenger with: " + tripData.participantKey, " Not Found!!!");
-
-
-            //update balance
-            passenger.balance -= fare;
-            transitProvider.balance += fare;
-            return passengerRegistry.update(passenger);
-
-            if (!tripLegId) {
-                // Successful update
-                var event = factory.newEvent('org.urbanstack.cto', 'CreateNewTripLeg');
-                event.tripId = tripData.tripId;
-                event.tripLegId = tripLegId;
-
-                emit(event);
-            } else trip.completedTripLegs.push(tripLegId);
-
-            var tripRegistry = {}
-            return getAssetRegistry('org.urbanstack.cto.Trip').then(function(registry) {
-                tripRegistry = registry
-                return tripRegistry.get(tripData.tripId);
-            }).then(function(trip) {
-                if (!trip) throw new Error("Trip with: " + tripData.tripId, " Not Found!!!");
-
-                trip.completedTripLegs.push(tripLegId);
-                return tripRegistry.update(trip);
-
-                // Successful update
-                var event = factory.newEvent('org.urbanstack.cto', 'QRScannedOnBus');
-                event.MiD = tripData.transitProviderKey;
-                event.origin = tripData.origin;
-                event.destination = tripData.destination;
-                event.fare = tripData.fare;
-                emit(event);
-            }).catch(function(error) {
-                throw new Error(error);
-            });
-        }).catch(function(error) {
-            throw new Error(error);
-        });
-        return transitProviderRegistry.update(transitProvider);
-    }).catch(function(error) {
-        throw new Error(error);
     });
 
+    return getParticipantRegistry('org.urbanstack.cto.Passenger').then(function(registry) {
+        passengerRegistry = registry
+        return passengerRegistry.get(passengerKey);
+    }).then(function(passenger) {
+        if (!passenger) throw new Error("Passenger with: " + tripData.participantKey, " Not Found!!!");
+
+        //update balance
+        passenger.balance -= fare;
+        return passengerRegistry.update(passenger);
+    });
+
+
+
+    if (!tripLegId) {
+        // Successful update
+        var event = factory.newEvent('org.urbanstack.cto', 'ConfirmTripLeg');
+        event.vPassengerId = tripData.vPassengerId;
+        event.tripLegId = tripLegId;
+        emit(event);
+    } else {
+        return getAssetRegistry('org.urbanstack.cto.vPassenger').then(function(registry) {
+            vPassengerRegistry = registry
+            return vPassengerRegistry.get(tripData.vPassengerId);
+        }).then(function(vPassenger) {
+            if (!vPassenger) throw new Error("vPassenger with: " + tripData.vPassengerId, " Not Found!!!");
+            // Successful update
+            var event = factory.newEvent('org.urbanstack.cto', 'QRScannedOnBus');
+            event.MiD = tripData.transitProviderKey;
+            event.origin = tripData.origin;
+            event.destination = tripData.destination;
+            event.fare = tripData.fare;
+            emit(event);
+
+            vPassenger.completedTripLegs.push(tripLegId);
+            return vPassengerRegistry.update(vPassenger);
+        })
+    }
+
+}
+
+/**
+ * Start vPassenger Transaction
+ * @param {org.urbanstack.ConfirmTripLeg} tripData
+ * @transaction
+ * Input:
+ * DateTime start_time
+ * TripLeg[] tentativeTripLegs
+ * String passengerKey
+ */
+function StartTrip(tripData) {
+    /**
+     * 1. Validate the start_time data
+     * If the date is a past date then throw an error
+     */
+    var timeNow = new Date().getTime();
+    var schedTime = new Date(tripData.start_time).getTime();
+    if (schedTime < timeNow) {
+        throw new Error("start_timed time cannot be in the past!!!");
+    }
+}
+
+/**
+ * End vPassenger Transaction
+ * @param {org.urbanstack.ConfirmTripLeg} tripData
+ * @transaction
+ * Input:
+ * DateTime start_time
+ * TripLeg[] tentativeTripLegs
+ * String passengerKey
+ */
+function EndTrip(tripData) {
+    /**
+     * 1. Validate the start_time data
+     * If the date is a past date then throw an error
+     */
+    var timeNow = new Date().getTime();
+    var schedTime = new Date(tripData.start_time).getTime();
+    if (schedTime < timeNow) {
+        throw new Error("start_timed time cannot be in the past!!!");
+    }
 }
