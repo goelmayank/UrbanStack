888f232aa66f4c9c18fb5abc289bd5b64e049872
thenaughtychid <f2014768@pilani.bits-pilani.ac.in>
2018-06-15 19:01:04 +0530

    firebase

--- FirebaseFunctions/functions/index.js
+++ FirebaseFunctions/functions/index.js
@@ -9,7 +9,7 @@ var gMapsClient = require('@google/maps').createClient({
 //
 
 /*
-Expects origin and destination from user
+Expects origin, destination and passengerKey from user
 Gets the geo json data using google directions api
 Should call hyperledger fabric rest server createTrip endpoint with following data:
 o Route overallRoute
@@ -18,13 +18,13 @@ o TripLeg[] tentativeTripLegs
 */
 exports.createTrip = functions.https.onRequest((req, res) => {
     try {
-        var data = req.query;
+        var req_query = req.query;
         var request = {
-            origin: data.origin,
-            destination: data.destination,
+            origin: req_query.origin,
+            destination: req_query.destination,
             mode: "transit"
         };
-        console.log('**********request data************\n', request)
+        console.log('**********request req_query************\n', request)
         console.log('finding tripLeg...')
         gMapsClient.directions(request, function(err, r) {
             console.log("inside directions callback", err, r)
@@ -32,29 +32,52 @@ exports.createTrip = functions.https.onRequest((req, res) => {
                 console.log("ERRRRRRRR", err);
                 res.status(401).send(err.toString());
             } else {
-                console.log("Response", r);
-
-                console.log("origin", r.json.routes[0].legs[0].steps[0].origin || "incorrect path");
-                console.log("destination", r.json.routes[0].legs[0].steps[0].destination || "incorrect path");
-                console.log("start_time", r.json.routes[0].legs[0].steps[0].start_time || "incorrect path");
-                console.log("end_time", r.json.routes[0].legs[0].steps[0].end_time || "incorrect path");
-                console.log("duration", r.json.routes[0].legs[0].steps[0].duration || "incorrect path");
-                console.log("distance", r.json.routes[0].legs[0].steps[0].distance || "incorrect path");
-                console.log("travel_mode", r.json.routes[0].legs[0].steps[0].travel_mode || "incorrect path");
-                console.log("fare", r.json.routes[0].legs[0].steps[0].fare || "incorrect path");
-
-                var tripLeg = {
-                    origin: r.json.routes[0].legs[0].steps[0].origin || "incorrect path",
-                    destination: r.json.routes[0].legs[0].steps[0].destination || "incorrect path",
-                    start_time: r.json.routes[0].legs[0].steps[0].start_time || "incorrect path",
-                    end_time: r.json.routes[0].legs[0].steps[0].end_time || "incorrect path",
-                    duration: r.json.routes[0].legs[0].steps[0].duration || "incorrect path",
-                    distance: r.json.routes[0].legs[0].steps[0].distance || "incorrect path",
-                    travel_mode: r.json.routes[0].legs[0].steps[0].travel_mode || "incorrect path",
-                    fare: r.json.routes[0].legs[0].steps[0].fare || "incorrect path"
+                console.log("Response", r.json);
+                // return res.status(200).json(r.json);
+                var json = r.json;
+                var overall_route = json.routes[0].legs[0];
+                var steps = json.routes[0].legs[0].steps;
+                console.log("origin", overall_route.origin || "incorrect path");
+                console.log("destination", overall_route.destination || "incorrect path");
+                console.log("start_time", overall_route.start_time || "incorrect path");
+                console.log("end_time", overall_route.end_time || "incorrect path");
+                console.log("duration", overall_route.duration || "incorrect path");
+                console.log("distance", overall_route.distance || "incorrect path");
+                console.log("travel_mode", overall_route.travel_mode || "incorrect path");
+                var route = {
+                    start_location: overall_route.start_location || "incorrect path",
+                    end_location: overall_route.end_location || "incorrect path",
+                    duration: overall_route.duration || "incorrect path",
+                    distance: overall_route.distance || "incorrect path",
+                    travel_mode: overall_route.travel_mode || "incorrect path",
                 };
-                console.log("** ** ** ** ** tripLeg ** ** ** ** ** ** \n ", tripLeg);
-                return res.status(200).json(tripLeg);
+                console.log("** ** ** ** ** route ** ** ** ** ** ** \n ", route);
+                var tripLegs = [];
+                for (let i = 0; i < steps.length; i++) {
+                    console.log("start_location", steps[i].origin || "incorrect path");
+                    console.log("start_location", steps[i].destination || "incorrect path");
+                    console.log("duration", steps[i].duration || "incorrect path");
+                    console.log("distance", steps[i].distance || "incorrect path");
+                    console.log("travel_mode", steps[i].travel_mode || "incorrect path");
+                    var tripLeg = {
+                        start_location: steps[i].start_location || "incorrect path",
+                        end_location: steps[i].end_location || "incorrect path",
+                        duration: steps[i].duration || "incorrect path",
+                        distance: steps[i].distance || "incorrect path",
+                        travel_mode: steps[i].travel_mode || "incorrect path",
+                    };
+                    console.log("** ** ** ** ** tripLeg ** ** ** ** ** ** \n ", tripLeg);
+                    tripLegs.push(tripLeg);
+                }
+                console.log("** ** ** ** ** tripLegs ** ** ** ** ** ** \n ", tripLegs);
+                var createTripJson = {
+                    overallRoute: route,
+                    tentativeTripLegs: tripLegs,
+                    passenger: 'org.urbanstack.Passenger#' + req_query.participantKey
+                }
+
+                console.log("** ** ** ** ** createTripJson ** ** ** ** ** ** \n ", createTripJson);
+                return res.status(200).json(createTripJson);
             }
         });
     } catch (e) {
