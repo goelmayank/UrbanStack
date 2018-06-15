2785b8352e5ca49beba08456c9512a579c99680f
thenaughtychid <f2014768@pilani.bits-pilani.ac.in>
2018-06-15 18:26:08 +0530

    firebase

--- FirebaseFunctions/functions/index.js
+++ FirebaseFunctions/functions/index.js
@@ -26,23 +26,32 @@ exports.createTrip = functions.https.onRequest((req, res) => {
         };
         console.log('**********request data************\n', request)
         console.log('finding tripLeg...')
-        gMapsClient.directions(request, function(err, response) {
-            console.log("inside directions callback", err, response)
+        gMapsClient.directions(request, function(err, r) {
+            console.log("inside directions callback", err, r)
             if (err) {
                 console.log("ERRRRRRRR", err);
                 res.status(401).send(err.toString());
             } else {
-                console.log("Response", response);
+                console.log("Response", r);
+
+                console.log("origin", r.json.routes[0].legs[0].steps[0].origin || "incorrect path");
+                console.log("destination", r.json.routes[0].legs[0].steps[0].destination || "incorrect path");
+                console.log("start_time", r.json.routes[0].legs[0].steps[0].start_time || "incorrect path");
+                console.log("end_time", r.json.routes[0].legs[0].steps[0].end_time || "incorrect path");
+                console.log("duration", r.json.routes[0].legs[0].steps[0].duration || "incorrect path");
+                console.log("distance", r.json.routes[0].legs[0].steps[0].distance || "incorrect path");
+                console.log("travel_mode", r.json.routes[0].legs[0].steps[0].travel_mode || "incorrect path");
+                console.log("fare", r.json.routes[0].legs[0].steps[0].fare || "incorrect path");
 
                 var tripLeg = {
-                    origin: r.routes[0].legs[0].steps[0].origin || "incorrect path",
-                    destination: r.routes[0].legs[0].steps[0].destination || "incorrect path",
-                    start_time: r.routes[0].legs[0].steps[0].start_time || "incorrect path",
-                    end_time: r.routes[0].legs[0].steps[0].end_time || "incorrect path",
-                    duration: r.routes[0].legs[0].steps[0].duration || "incorrect path",
-                    distance: r.routes[0].legs[0].steps[0].distance || "incorrect path",
-                    travel_mode: r.routes[0].legs[0].steps[0].travel_mode || "incorrect path",
-                    fare: r.routes[0].legs[0].steps[0].fare || "incorrect path"
+                    origin: r.json.routes[0].legs[0].steps[0].origin || "incorrect path",
+                    destination: r.json.routes[0].legs[0].steps[0].destination || "incorrect path",
+                    start_time: r.json.routes[0].legs[0].steps[0].start_time || "incorrect path",
+                    end_time: r.json.routes[0].legs[0].steps[0].end_time || "incorrect path",
+                    duration: r.json.routes[0].legs[0].steps[0].duration || "incorrect path",
+                    distance: r.json.routes[0].legs[0].steps[0].distance || "incorrect path",
+                    travel_mode: r.json.routes[0].legs[0].steps[0].travel_mode || "incorrect path",
+                    fare: r.json.routes[0].legs[0].steps[0].fare || "incorrect path"
                 };
                 console.log("** ** ** ** ** tripLeg ** ** ** ** ** ** \n ", tripLeg);
                 return res.status(200).json(tripLeg);
